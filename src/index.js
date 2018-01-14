9b3d0d6ede0c994c0ceca0c67789bd15da9150ab
Silber <silber.ypm@gmail.com>
2018-01-14 17:46:37 +0500

    static PropTypes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component{
 
+    static propTypes = {
+        btnText: React.PropTypes.string.isRequired,
+        h1Text: React.PropTypes.string.isRequired
+    };
+
     btnOnClick(event) {
         console.log('Button clicked', event.target);
     }
@@ -14,15 +19,15 @@ class App extends React.Component{
     render() {
         return (
             <div className="test">
-                <h1>App works!</h1>
+                <h1> { this.props.h1Text } </h1>
                 <p>what's up world?!</p>
-                <button onClick={this.btnOnClick}>{ this.props.btnText }</button>
+                <button onClick={this.btnOnClick}>{ this.props.btnText  || "Default text"}</button>
             </div>
         );
     }
 }
 
 ReactDOM.render( 
-    <App btnText="Click on me!" />,
+    <App btnText="Click on me!" h1Text="This is h1 text" />,
     document.getElementById('app')
 );
